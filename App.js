8181947ed1c6bc19232cf7dc81dd5465cd0c6414
Ankit Kumar <ankitkumar11012001@gmail.com>
2026-02-17 22:49:04 +0530

    Episode 3 Complete

--- App.js
+++ App.js
@@ -1,27 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-/**
- * 
- * <div id="parent">
- *      <div id="child">
- *          <h1>I'm an H1 Tag</h1>
- *          <h1>I'm an H1 Tag again !</h1>
- *      </div>
- * </div>
- */
 
-const parent = React.createElement("div", { id: "parent" }, 
-    React.createElement("div", { id: "parent" }, 
-        [
-            React.createElement("h1", {key: "child1"}, "I am an H1 Tag"),
-            React.createElement("h1", {key: "child2"}, "I am an H1 Tag again!")
-        ]
-    )
+const elem = <span>This is a span</span>;
+
+// if we have to write multi-lind JSX, then wrap it in simple brackets '()'
+const jsxHeading = (
+    <h1 id="headingJSX" className="head" tabIndex="1">
+        {elem}
+        React heading using JSX!!
+    </h1>
 );
 
-// JSX
+console.log(jsxHeading); // This will be same as the heading using React.createElement
 
-console.log(parent);
-const root = ReactDOM.createRoot(document.getElementById("root")); // ReactDOM is used to manipulate the DOM in the browser
+const number = 1000;
 
-root.render(parent);
+// React Functional Components
+// This is similar to JS function(we can use lambda or a normal JS function both) and it just returns JSX
+// We can put any JS expression using curly braces in JSX, so now we can just put this variable in the curly brace and thus we can render jsx as well inside the component
+const HeadingComponent = () => {
+    return (
+        <div id="container">
+            <h2>{number}</h2>
+            {jsxHeading}
+            <h1>This is a functional React Component</h1>
+        </div>
+    );
+};
+
+// Component Composition, we can just call the function directly as well, as shown in the below example
+const Title = () => {
+    return (
+        <div id="titleContainer">
+            <h1>Title Functional Component</h1>
+            <HeadingComponent />
+            <HeadingComponent></HeadingComponent>
+            {HeadingComponent()}
+        </div>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+// root.render(jsxHeading); // syntax to render jsxHeading
+root.render(<Title />);
+
--- README.md
+++ README.md
@@ -18,3 +18,29 @@
 - HTTPS
 - Tree Shaking - Remove unused code from files
 - Different dev and prod bundles (npx parcel build index.html)
+
+## JSX
+- JSX is not writing HTML in JS, it's a HTML (or) XML like syntax.
+- Creating element using React.createElement is very difficult, so JSX is introduced to simplify this.
+Sample for React.createElement:
+```
+React.createElement(
+    "h1", 
+    {id: "heading"}, 
+    "this is an H1 Tag"
+);
+```
+- JSX code is not pure JS since it's not following ES6, Parcel is transpiling the code using Babel and then it is read by the browser.
+- Sanitizes the data as well if we try to run some malicious code in the components. 
+
+## Babel
+- Babel is a JS compiler.
+- Babel transpiles the code so it can be rendered into the browsers.
+
+## React Component
+- There are two types of components in react
+    1. Class Based Components - OLD
+    2. Fucntional Components - NEW => Same as JS functions
+    - Anything JS function which just returns JSX, it's called as React Functional Components.
+- Eveerything is a component in a React.
+- Component Composition -> Rendering component inside another component.
